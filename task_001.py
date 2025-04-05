c4e793f1ae32fda3231dedcab417f93f9f75740f
Somyagoyal01 <150276014+Somyagoyal01@users.noreply.github.com>
2025-04-05 06:41:53 +0530

    Update task_001.py

--- task_001.py
+++ task_001.py
@@ -2,7 +2,11 @@
 1.   Opens and reads a text file named sample.txt.
 2.   Prints its content line by line.
 3.   Handles errors gracefully if the file does not exist."""
-file1= open('sample.txt','r')
-reading_file = file1.read()
-print(reading_file)
-file1.close()
+try:
+    file1= open('sample.txt','r')
+    reading_file = file1.read()
+except FileNotFoundError:
+     print("The file 'sample.txt' does not exist.")
+finally:
+     print(reading_file)
+     file1.close()
